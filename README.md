a5257a274a1e2cdc243bc3c59ee9fe2dc44594d2
Yuliya Khilko <ykhilko@gmail.com>
2020-05-12 13:48:52 -0700

    feat: apply yandex code formating

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 ### Overview
 
-* Figma
-* Images
+- Figma
+- Images
 
 **Figma**
 
-* [Link to the project in Figma](https://www.figma.com/file/xM9rNsdK4iNcFJmDZho3Aw/Sprint-3%3A-From-Portland-to-Portland-%2F-desktop-%2B-mobile?node-id=500%3A0)
+- [Link to the project in Figma](https://www.figma.com/file/xM9rNsdK4iNcFJmDZho3Aw/Sprint-3%3A-From-Portland-to-Portland-%2F-desktop-%2B-mobile?node-id=500%3A0)
 
 **Images**
 
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
+The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster.
 
 Have fun with JavaScript!
--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,31 @@
-
-const popup = document.querySelector(".js-popup");
-const editBtn = document.querySelector(".js-edit-btn");
+const popup = document.querySelector('.js-popup');
+const editBtn = document.querySelector('.js-edit-btn');
 const form = document.querySelector(".js-form");
-const closeBtn = popup.querySelector(".js-close-btn");
-const userName = document.querySelector(".js-user-name");
-const userJob = document.querySelector(".js-user-job");
-const inputName = popup.querySelector("input[name=name]");
-const inputJob = popup.querySelector("input[name=job]");
-
+const closeBtn = popup.querySelector('.js-close-btn');
+const userName = document.querySelector('.js-user-name');
+const userJob = document.querySelector('.js-user-job');
+const inputName = popup.querySelector('input[name=name]');
+const inputJob = popup.querySelector('input[name=job]');
 
 function togglePopup() {
-  popup.classList.toggle("popup_visible");
+  popup.classList.toggle('popup_visible');
 }
 
-editBtn.addEventListener("click", function(){
+editBtn.addEventListener('click', function () {
   inputName.value = userName.textContent;
   inputJob.value = userJob.textContent;
   togglePopup();
 });
 
-closeBtn.addEventListener("click",function(){
-  inputName.value = "";
-  inputJob.value = "";
+closeBtn.addEventListener('click', function () {
+  inputName.value = '';
+  inputJob.value = '';
   togglePopup();
 });
 
-form.addEventListener("submit", function(e){
-e.preventDefault();
-userName.textContent = inputName.value;
-userJob.textContent = inputJob.value;
-togglePopup();
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  userName.textContent = inputName.value;
+  userJob.textContent = inputJob.value;
+  togglePopup();
 });
