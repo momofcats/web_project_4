508a94528097a210a4209274ce78d9c64bd3a8d5
Yuliya Khilko <ykhilko@gmail.com>
2020-06-22 15:26:43 -0700

    feat: move card to module

---
+++ scripts/Card.js
@@ -0,0 +1,74 @@
+//popup data
+
+const popupPicture = document.querySelector(".js-popup-picture");
+const popupImage = popupPicture.querySelector(".popup__image");
+const popupTitle = popupPicture.querySelector(".popup__title");
+
+function togglePopup(element) {
+    element.classList.toggle("popup_role_show");
+  }
+
+  function animateFadeOut(element) {
+    element.classList.add("popup_role_fade-out");
+  }
+
+class Card {
+  constructor(data, templateSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._templateSelector = templateSelector;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._card = this._getTemplate();
+    this._card.querySelector(
+      ".card__img"
+    ).style.backgroundImage = `url('${this._link}')`;
+    this._card.querySelector(".card__title").textContent = this._name;
+    this._setEventListeners();
+    return this._card;
+  }
+
+  _handleLikeBtnClick(evt) {
+    const target = evt.target;
+    target.classList.toggle("card__like_active");
+  }
+
+  _handleDelBtnClick(evt) {
+    const cardElement = evt.target.parentNode;
+    cardElement.remove(evt);
+  }
+
+  _handleOpenPopup() {
+    popupImage.src = "";
+    popupImage.src = this._link;
+    popupImage.alt = this._name;
+    popupTitle.textContent = this._name;
+    togglePopup(popupPicture);
+    animateFadeOut(popupPicture);
+
+  }
+
+  _setEventListeners() {
+    this._card.querySelector(".card__like").addEventListener("click", (evt) => {
+      this._handleLikeBtnClick(evt);
+    });
+    this._card.querySelector(".card__del").addEventListener("click", (evt) => {
+      this._handleDelBtnClick(evt);
+    });
+    this._card.querySelector(".card__img").addEventListener("click", () => {
+      this._handleOpenPopup();
+    });
+  }
+}
+
+export {Card, popupPicture, togglePopup, animateFadeOut};
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,9 @@
 import FormValidator from "./FormValidator.js";
+import {Card, popupPicture, togglePopup, animateFadeOut} from "./Card.js";
 
-const settings = {
-  inputSelector: ".form__input",
-  submitButtonSelector: ".form__submit-btn",
-  inactiveButtonClass: "form__submit-btn_inactive",
-  inputErrorClass: "form__input_type_error",
-  errorClass: "form__input-error_active",
-}
+//templates
 
-
-
+const cardTemplateSelector = ".js-card-template";
 
 //wrappers
 
@@ -21,7 +15,7 @@ const cardForm = popupCard.querySelector(".form");
 
 const gallery = document.querySelector(".gallery");
 
-const popupPicture = document.querySelector(".js-popup-picture");
+
 
 //buutons and DOM elements
 const editBtn = document.querySelector(".media__btn");
@@ -43,9 +37,8 @@ const inputLink = cardForm.querySelector(".js-input-link");
 const userName = document.querySelector(".media__name");
 const userJob = document.querySelector(".media__job");
 
-//popup data
-const popupImage = popupPicture.querySelector(".popup__image");
-const popupTitle = popupPicture.querySelector(".popup__title");
+
+
 
 const initialCards = [
   {
@@ -74,12 +67,17 @@ const initialCards = [
   },
 ];
 
-function togglePopup(element) {
-  element.classList.toggle("popup_role_show");
-}
+const settings = {
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__submit-btn",
+  inactiveButtonClass: "form__submit-btn_inactive",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+};
 
-function animateFadeOut(element) {
-  element.classList.add("popup_role_fade-out");
+function renderCard(data) {
+  const card = new Card(data, cardTemplateSelector);
+  gallery.prepend(card.generateCard());
 }
 
 function closePopupOnEsc(evt) {
@@ -93,7 +91,7 @@ function closePopupOnEsc(evt) {
 
 function closePopupOnClick(evt) {
   const targetPopup = evt.target;
-  if(!targetPopup.classList.contains("popup_role_show")) {
+  if (!targetPopup.classList.contains("popup_role_show")) {
     return;
   }
   togglePopup(targetPopup);
@@ -135,9 +133,7 @@ cardForm.addEventListener("submit", (evt) => {
     name: inputTitle.value,
     link: inputLink.value,
   };
-  const cardInstance = new Card(card, ".js-card-template");
-  const cardElement = cardInstance.generateCard();
-  gallery.prepend(cardElement);
+  renderCard(card);
   togglePopup(popupCard);
 });
 
@@ -146,81 +142,18 @@ picturePopupCloseBtn.addEventListener("click", () => {
   animateFadeOut(popupPicture);
 });
 
-
-class Card {
-  constructor(data, templateSelector) {
-    this._name = data.name;
-    this._link = data.link;
-    this._templateSelector = templateSelector;
-  }
-
-  _getTemplate() {
-    const cardElement = document
-      .querySelector(this._templateSelector)
-      .content.cloneNode(true);
-
-    return cardElement;
-  }
-
-  generateCard() {
-    this._element = this._getTemplate();
-    this._element.querySelector(
-      ".card__img"
-    ).style.backgroundImage = `url('${this._link}')`;
-    this._element.querySelector(".card__title").textContent = this._name;
-    this._setEventListeners();
-    return this._element;
-  }
-
-  _handleLikeBtnClick(evt) {
-    const target = evt.target;
-    target.classList.toggle("card__like_active");
-  }
-
-  _handleDelBtnClick(evt) {
-    const cardElement = evt.target.parentNode;
-    cardElement.remove(evt);
-  }
-
-  _handleOpenPopup() {
-    popupImage.src = "";
-    popupImage.src = this._link;
-    popupImage.alt = this._name;
-    popupTitle.textContent = this._name;
-    togglePopup(popupPicture);
-    animateFadeOut(popupPicture);
-  }
-
-  _setEventListeners() {
-    this._element
-      .querySelector(".card__like")
-      .addEventListener("click", (evt) => {
-        this._handleLikeBtnClick(evt);
-      });
-    this._element
-    .querySelector(".card__del")
-    .addEventListener("click", (evt) => {
-      this._handleDelBtnClick(evt);
-    });
-    this._element
-    .querySelector(".card__img")
-    .addEventListener("click", () => {
-      this._handleOpenPopup();
-    });
-  }
-}
-
 document.addEventListener("click", closePopupOnClick);
 document.addEventListener("keydown", closePopupOnEsc);
 
+// Render gallery
+
 initialCards.forEach((item) => {
-  const card = new Card(item, ".js-card-template");
-  const cardElement = card.generateCard();
-  gallery.append(cardElement);
+  renderCard(item);
 });
 
+// Validate forms
 
 const validatedProfileForm = new FormValidator(settings, profileForm);
-  const validatedCardForm = new FormValidator(settings, cardForm);
-  validatedProfileForm.enableValidation();
-  validatedCardForm.enableValidation();
+const validatedCardForm = new FormValidator(settings, cardForm);
+validatedProfileForm.enableValidation();
+validatedCardForm.enableValidation();
