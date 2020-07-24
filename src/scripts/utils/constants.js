401a54efb50ed535891a4cae43eae68d3e483d61
Yuliya Khilko <ykhilko@gmail.com>
2020-07-24 15:44:41 -0700

    feat: remove initial cards arr

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -1,34 +1,5 @@
 //templates
-
 export const cardTemplateSelector = ".js-card-template";
-
-//data
-export const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanois National Park",
-    link: "https://code.s3.yandex.net/web-code/vanois.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
 
 //buutons and DOM elements
 const popupProfile = document.querySelector(".js-popup-profile");
